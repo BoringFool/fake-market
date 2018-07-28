9207b8f13c024584e29081dbdac3b558b1e79a15
BoringFool <1318593330@qq.com>
2018-07-28 08:11:59 +0800

    .
    sessionlistener   session销毁时，转存shoppingcart中的信息到数据库

--- WebContent/js/cartShow.js
+++ WebContent/js/cartShow.js
@@ -1,76 +1,102 @@
 $(document).ready(function() {
-			$("button#sub").click(function() {
-						zm();
+	$("button#sub").click(function() {
+				cartshow.zm();
 
-					});
+			});
 
-			
-			function CartShow(){
+	function CartShow() {
+	}
+
+	var cartshow = new CartShow();
+	CartShow.prototype.aj = function() {
+		var da = {
+			"orderNumber" : {
+				1 : 245,
+				2 : 24
 			}
-			
-			var cartshow=new CartShow();
-			CartShow.prototype.aj=function () {
-				var da = {
-					"orderNumber" : {
-						1 : 245,
-						2 : 24
+		};
+
+		$.ajax({
+					type : "post",
+					url : "http://localhost:8080/fake_market/orderlist/paylist",
+					data : JSON.stringify(da),
+					contentType : "application/json;charset=utf-8",
+					dataType : "json",
+					success : function(data) {
+						alert(data);
+					},
+					error : function() {
+						alert("false");
 					}
-				};
 
-				$.ajax({
-							type : "post",
-							url : "http://localhost:8080/fake_market/orderlist/paylist",
-							data : JSON.stringify(da),
-							contentType : "application/json;charset=utf-8",
-							dataType : "json",
-							success : function(data) {
-								alert(data);
-							},
-							error : function() {
-								alert("false");
-							}
+				});
 
-						});
+	}
 
-			}
-			
-			CartShow.prototype.checkShow=function (){
-				alert(2);
-				$.ajax({
-					type:"post",
-					url:"http://localhost:8080/fake_market/orderlist/aaa",
-					dataType:"json",
-					success:function(data){
-						if(data[0]==0){
+	CartShow.prototype.checkShow = function() {
+		alert(2);
+		$.ajax({
+					type : "post",
+					url : "http://localhost:8080/fake_market/orderlist/aaa",
+					dataType : "json",
+					success : function(data) {
+						if (data[0] == 0) {
 							alert("暂无物品在购物车中");
-						}else{
-							for(var key in data){
-								alert("物品id："+ key+"物品数量："+data[key]);
+						} else {
+							for (var key in data) {
+								alert("物品id：" + key + "物品数量：" + data[key]);
 							}
 						}
-						
-							
-						
+
 					},
-					error:function(){}
+					error : function() {
+					}
 				})
-			}
-			
-			
-			function zm(){
-				$.ajax({
-					type:"post",
-					url:"http://localhost:8080/fake_market/goods/cartShow",
-					dataType:"json",
-					success:function(data){
-						for(var i in data){
-							alert(data[i][0]);
+	}
+
+	CartShow.prototype.models = function() {
+		var param = arguments[0];
+
+		var model = "<div class=\"showList\" >" + "<input type=\"checkbox\" >"
+				+ "<div class=\"pic\">" + "	<img alt=\"\" src=\"" + param[0]
+				+ "\">" + "</div>" + "<div class=\"titleM\">" + "	<span>"
+				+ param[1] + "</span><br/>" + "	<span>" + param[2] + "</span>"
+				+ "</div>" + "<div class=\"priceM\">" + param[3] + "</div>"
+				+ "<div class=\"numberM\">" + param[4] + "</div>"
+				+ "<div class=\"soldM\">" + "	<span>申请售后</span>" + "</div>"
+				+ "<div class=\"countM\">" + "	<span>" + param[3] * param[4]
+				+ "</span>" + "</div>" + "<div class=\"stateM\">"
+				+ "	<span>交易成功</span>" + "</div>" + "<div class=\"reviewM\">"
+				+ "	<span>评论</span>" + "</div>" + "<div class=\"logisticsM\">"
+				+ "	<span>查看物流</span>" + "</div>" + "</div>";
+		var nullDiv = "<div class=\"nullDiv\">最近未添加商品 </div>";
+		if (param == false) {
+			$("div.newList").append(nullDiv);
+		} else {
+			$("div.newList").append(model);
+		}
+
+	}
+
+	CartShow.prototype.zm = function() {
+		$.ajax({
+					type : "post",
+					url : "http://localhost:8080/fake_market/goods/cartShow",
+					dataType : "json",
+					success : function(data) {
+
+						for (var i in data) {
+							if (data[i][0] == "0" && data[i][1] == "0"
+									&& data[i][2] == "0") {
+								cartshow.models(false);
+								break;
+							}
+							cartshow.models(data[i]);
 						}
-						
-							
-						
 					},
-					error:function(){}
+					error : function() {
+					}
 				})
-			}
-		});
+
+	}
+});
--- WebContent/jsp/cartShow.jsp
+++ WebContent/jsp/cartShow.jsp
@@ -16,87 +16,13 @@
 			<div  class="showHead" style="color: #FF6000">未使用</div>
 			<div class="showHead">已购买</div>
 		</div>
-		<div style="width: 1244px;border: 1px solid red;float: left;">
+		<div class="newList">
 			
-			<div style="width: 1220px;height: 100px;overflow: hidden;border: 1px solid #F1F1F1;margin: 10px;font-size: 12px;">
-				<input type="checkbox" style="display: inline-block;float: left;margin: 44px 4px;">
-				<div style="width: 80px;height:84px;float: left;padding: 8px">
-					<img alt="" src="../image/goods_pic.jpg" style="width: 72px;height:72px;">
-				</div>
-				<div style="float: left;width: 300px;height:92px;padding:  4px;">
-					<span>data.name</span><br/>
-					<span>data.store</span>
-				</div>
-				<div style="float: left;width: 100px;height:92px;padding:  4px;">
-					data.price
-				</div>
-				<div style="float: left;width: 50px;height:92px;padding:  4px;">
-					data.number
-				</div>
-				<div style="float: left;width: 120px;height:92px;padding:  4px;border-right: 1px solid #F1F1F1">
-					<span>申请售后</span>
-					<span></span>
-					
-				</div>
-				<div style="float: left;width: 120px;height:92px;padding:  4px;border-right: 1px solid #F1F1F1">
-					<span>number*price</span>
-				</div>
-				<div style="float: left;width: 120px;height:92px;padding:  4px;border-right: 1px solid #F1F1F1">
-					<span>交易成功</span>
-					
-				</div>
-				<div style="float: left;width: 120px;height:92px;padding:  4px;border-right: 1px solid #F1F1F1">
-					<span>评论</span>
-					
-				</div>
-				<div style="float: left;width: 104px;height:92px;padding:  4px;">
-					<span>查看物流</span>
-					
-				</div>
-			</div>
-			
-			<div style="width: 1220px;height: 100px;overflow: hidden;border: 1px solid #F1F1F1;margin: 10px;font-size: 12px;">
-				<input type="checkbox" style="display: inline-block;float: left;margin: 44px 4px;">
-				<div style="width: 80px;height:84px;float: left;padding: 8px">
-					<img alt="" src="../image/goods_pic.jpg" style="width: 72px;height:72px;">
-				</div>
-				<div style="float: left;width: 300px;height:92px;padding:  4px;">
-					<span>data.name</span><br/>
-					<span>data.store</span>
-				</div>
-				<div style="float: left;width: 100px;height:92px;padding:  4px;">
-					data.price
-				</div>
-				<div style="float: left;width: 50px;height:92px;padding:  4px;">
-					data.number
-				</div>
-				<div style="float: left;width: 120px;height:92px;padding:  4px;border-right: 1px solid #F1F1F1">
-					<span>申请售后</span>
-					<span></span>
-					
-				</div>
-				<div style="float: left;width: 120px;height:92px;padding:  4px;border-right: 1px solid #F1F1F1">
-					<span>number*price</span>
-				</div>
-				<div style="float: left;width: 120px;height:92px;padding:  4px;border-right: 1px solid #F1F1F1">
-					<span>交易成功</span>
-					
-				</div>
-				<div style="float: left;width: 120px;height:92px;padding:  4px;border-right: 1px solid #F1F1F1">
-					<span>评论</span>
-					
-				</div>
-				<div style="float: left;width: 104px;height:92px;padding:  4px;">
-					<span>查看物流</span>
-					
-				</div>
-			</div>
-		
 		</div>
-		<div>
+		<div style="display: none;">
 			2
 		</div>
-		<div>
+		<div style="display: none;">
 			3
 		</div>
 	
