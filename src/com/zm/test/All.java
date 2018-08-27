160f1196598542ece72fd194caf859347646a053
BoringFool <1318593330@qq.com>
2018-08-28 06:41:11 +0800

    .

--- src/com/zm/test/All.java
+++ src/com/zm/test/All.java
@@ -4,19 +4,27 @@ import java.io.UnsupportedEncodingException;
 
 import org.junit.Test;
 
-
 public class All {
 
 	@Test
 	public void test() throws UnsupportedEncodingException {
 		String a = "������ʾ��Course��һ��������ά��";
-		String news=new String(a.getBytes("gb2312"),"UTF-8");
+		String news = new String(a.getBytes("gb2312"), "UTF-8");
 		System.out.println(news);
 	}
+
 	@Test
 	public void testBefore() {
-		
+		String s1 = "Programming";
+		String s2 = new String("Programming");
+		String s3 = "Program" + "ming";
+		System.out.println(s1 == s2);
+		System.out.println(s1 == s3);
+		System.out.println(s1 == s1.intern());
 	}
 
-	
+	@Test
+	public void test_e() {
+		
+	}
 }
---
+++ src/com/zm/test/Human.java
@@ -0,0 +1,29 @@
+package com.zm.test;
+
+@SuppressWarnings("serial")
+class Annoyance extends Exception {
+}
+
+@SuppressWarnings("serial")
+class Sneeze extends Annoyance {
+}
+
+public class Human {
+
+	public static void main(String[] args) throws Exception {
+		try {
+			try {
+				throw new Sneeze();
+			} catch (Annoyance a) {
+				System.out.println("Caught Annoyance");
+				throw a;
+			}
+		} catch (Sneeze s) {
+			System.out.println("Caught Sneeze");
+			return;
+		} finally {
+			System.out.println("Hello World!");
+		}
+	}
+
+}
