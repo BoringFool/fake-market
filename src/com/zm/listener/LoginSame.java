7fe5f14c3141cdabb3d44c1166f1d96a22807bbe
BoringFool <1318593330@qq.com>
2018-08-30 09:38:29 +0800

    .
    replaced方法怎么去除被替代sessionattribute有点问题，需要再想一下。

---
+++ src/com/zm/listener/LoginSame.java
@@ -0,0 +1,74 @@
+package com.zm.listener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Application Lifecycle Listener implementation class LoginSame
+ *
+ */
+@WebListener
+public class LoginSame implements HttpSessionAttributeListener, HttpSessionListener {
+	Map<String, HttpSession> map = new HashMap<String, HttpSession>();
+
+	// 属性增加
+	public void attributeAdded(HttpSessionBindingEvent arg0) {
+		String name = arg0.getName();
+		if ("username".equals(name)) {
+			String userName = (String) arg0.getValue();
+			if (map.get(userName) != null) {
+				HttpSession session = map.get(userName);
+				session.removeAttribute("username");
+			}
+			map.put((String) arg0.getValue(), arg0.getSession());
+		}
+	}
+
+	// 属性删除
+	public void attributeRemoved(HttpSessionBindingEvent arg0) {
+		String name = arg0.getName();
+		if (name.equals("username")) {
+			String ub = (String) arg0.getValue();
+			map.remove(ub);
+		}
+	}
+
+	// 属性替换
+	public void attributeReplaced(HttpSessionBindingEvent arg0) {
+		String name = arg0.getName();
+		if (name.equals("username")) {
+			String oldub = (String) arg0.getValue();
+			map.remove(oldub);
+			String ub = (String) arg0.getSession().getAttribute("username");
+			if (map.get(ub) != null) {
+				// map中有记录，表明该帐号在其他机器上登录过，将以前的登录失效
+				HttpSession session = map.get(ub);
+				session.removeAttribute("username");
+			}
+			map.put(ub, arg0.getSession());
+		}
+	}
+
+	@Override
+	public void sessionCreated(HttpSessionEvent se) {
+		HttpSessionListener.super.sessionCreated(se);
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent se) {
+		try {
+			String userName = (String) se.getSession().getAttribute("username");
+			map.remove(userName);
+		} catch (Exception e) {
+			System.out.println("当前session没有登陆！");
+		}
+	}
+
+}
--- src/com/zm/listener/TestListener.java
+++ src/com/zm/listener/TestListener.java
@@ -12,13 +12,11 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.zm.service.IOrderListService;
 
-
 @WebListener
 public class TestListener implements HttpSessionListener {
 
-	private  int userNumber = 0;
+	private int userNumber = 0;
 
-	
 	public TestListener() {
 
 	}
@@ -26,34 +24,23 @@ public class TestListener implements HttpSessionListener {
 	@Override
 	public void sessionCreated(HttpSessionEvent sessionevent) {
 		userNumber++;
-		sessionevent.getSession().setAttribute("userNumber", userNumber);
+		sessionevent.getSession().getServletContext().setAttribute("userNumber", userNumber);
 
 	}
 
 	@SuppressWarnings({ "unchecked", "unused" })
 	@Override
 	public void sessionDestroyed(HttpSessionEvent sessionevent) {
-		HttpSession session=sessionevent.getSession();
-		IOrderListService configService = (IOrderListService) WebApplicationContextUtils.getWebApplicationContext(session.getServletContext()).getBean("orderlistservice");
+		HttpSession session = sessionevent.getSession();
+		IOrderListService configService = (IOrderListService) WebApplicationContextUtils
+				.getWebApplicationContext(session.getServletContext()).getBean("orderlistservice");
 
-		
-		
-		
-		
-		
-		
-		if(session.getAttribute("shoppingCart")==null) {
-			
-		}else {
-			Map<Long, Integer> shoppingCart = (HashMap<Long, Integer>)session.getAttribute("shoppingCart");
+		if (session.getAttribute("shoppingCart") == null) {
+
+		} else {
+			Map<Long, Integer> shoppingCart = (HashMap<Long, Integer>) session.getAttribute("shoppingCart");
 		}
-		
 
 	}
 
-	
-	
-	
-	
-	
 }
